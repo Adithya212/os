16cfeb820ddbd70bbf726c370fa777f613950074
Adithya212 <81566547+Adithya212@users.noreply.github.com>
2022-10-04 18:50:05 +0530

    Add files via upload

---
+++ cscan_disk.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+
+int main()
+{
+    int initpos, nreq, i, j, temp, currentpos, maxpos, secpos = -1, tothdmov = 0;
+    printf("Enter the initial position of the head:");
+    scanf("%d", &initpos);
+    currentpos = initpos;
+    printf("Enter the number of I/O requests:");
+    scanf("%d", &nreq);
+    int arr[50];
+    printf("Enter the maximum position:");
+    scanf("%d", &maxpos);
+    printf("Enter the I/O requests:");
+    for(i=0;i<nreq;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("\n ARRAY IS: \n");
+    for(i=0;i<nreq;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    
+    //sorting the array
+    for(i=0;i<nreq;i++)
+    {
+        for(j=0;j<nreq-i-1;j++)
+        {
+            if(arr[j]>arr[j+1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    
+    printf("\nSORTED ARRAY IS: \n");
+    for(i=0;i<nreq;i++)
+    {
+        printf("%d\t",arr[i]);
+    }
+    //traversing from initial head position to highest I/O request position
+    for(i=0;i<nreq;i++)
+    {
+        if(arr[i]<initpos)
+        {
+            secpos = i;
+        }
+        else if (arr[i]>initpos)
+        {
+            tothdmov += arr[i]-currentpos;
+            printf("       %d     \n", arr[i]-currentpos);
+            printf("%d--------->%d\n", currentpos, arr[i]);
+            currentpos = arr[i];
+        }
+    }
+    //travelling to the end of the disk
+    if(arr[i]!=maxpos)
+    {
+        tothdmov += maxpos-currentpos;
+        printf("       %d     \n", maxpos-currentpos);
+        printf("%d--------->%d\n", currentpos, maxpos);
+        currentpos = maxpos;
+    }
+    //travelling to the begining of the disk
+    tothdmov += maxpos;
+    printf("       %d     \n", maxpos);
+    printf("%d--------->%d\n", currentpos, 0);
+    currentpos = 0;
+    //traversing fromm 0th position
+    for(i=0;i<=secpos;i++)
+    {
+        tothdmov += arr[i]-currentpos;
+        printf("       %d     \n", arr[i]-currentpos);
+        printf("%d--------->%d\n", currentpos, arr[i]);
+        currentpos = arr[i];
+    }
+    printf("The total head movement=%d\n", tothdmov);
+}
