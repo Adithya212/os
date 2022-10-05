a60b3963d954cbffd633bb914d68a8c371289bca
Adithya212 <81566547+Adithya212@users.noreply.github.com>
2022-10-05 19:53:04 +0530

    Update priority.c

--- priority.c
+++ priority.c
@@ -1,78 +1,68 @@
 
-#include <stdio.h>
-#define MAX_SIZE 50
-
-void bubblesort(int arr[],int arr2[],int arr3[],int arr4[],int size);
-void swap(int *a,int *b);
-int main(){
-    int n,time=0;
-    float avgwt=0,avgtat=0;
-    int PRIORITY[MAX_SIZE];
-    int AT[MAX_SIZE],BT[MAX_SIZE],CT[MAX_SIZE],WT[MAX_SIZE],TAT[MAX_SIZE];
-    int P[MAX_SIZE];
-    printf("Enter the number of processes : ");
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+    int et[20],at[10],n,i,j,temp,p[10],st[10],ft[10],wt[10],ta[10];
+    int totwt=0,totta=0;
+    float awt,ata;
+    char pn[10][10],t[10];
+    //clrscr();
+    printf("Enter the number of process:");
     scanf("%d",&n);
-
-    printf("\nEnter the Arrival Time ,Burst Time and Priority for the following processes\n");
- 
-    for(int i =0; i<n; i++){
-        printf("\np[%d]",i+1);
-        printf("\nArrival Time:");
-        scanf("%d",&AT[i]);
-        printf("Burst Time:");
-        scanf("%d",&BT[i]);
-        printf("Priority:");
-        scanf("%d",&PRIORITY[i]);
-        P[i] = i+1;
+    for(i=0; i<n; i++)
+    {
+        printf("Enter process name,arrivaltime,execution time & priority:");
+        //flushall();
+        scanf("%s%d%d%d",pn[i],&at[i],&et[i],&p[i]);
     }
-    bubblesort(PRIORITY,BT,P,AT,n);
-    time = AT[0]; // initial time 
-    for(int i =0;i<n;i++){
-        time += BT[i]; // increasing the Time
-
-        CT[i] = time;   
-        TAT[i] = CT[i] - AT[i];
-        WT[i] = TAT[i]-BT[i];
-
-        avgtat+=TAT[i];
-        avgwt+=WT[i];
-    }
-    avgtat /= n;
-    avgwt /= n;
-
-    printf("\nProcess\tPriority\tArrival Time\tBurst Time\tCompletion\tTurnAround\tWaiting\n");
-    for(int i=0;i<n;i++){
-        printf("P[%d]\t",P[i]);
-        printf("%d\t",PRIORITY[i]);
-        printf("\t%d\t",AT[i]);
-        printf("\t%d\t",BT[i]);
-        printf("\t%d\t",CT[i]);
-        printf("\t%d\t",TAT[i]);
-        printf("\t%d\t\n",WT[i]);
-
-
-    }
-    printf("\nAverage TurnAroundTime : %f",avgtat);
-    printf("\nAverage WaitingTime : %f",avgwt);
-
-    return 0;
-}
-void swap(int *a,int *b){
-    int temp;
-    temp = *a;
-    *a = *b;
-    *b = temp;
-}
-void bubblesort(int arr[],int arr2[],int arr3[],int arr4[],int size){
-    for (int i = 0; i < size-1; i++){
-        int temp = 0;
-        for (int j = 0; j < size - i - 1; j++){
-            if (arr[j] > arr[j + 1]){ 
-                swap(&arr[j],&arr[j+1]);
-                swap(&arr2[j],&arr2[j+1]);
-                swap(&arr3[j],&arr3[j+1]);
-                swap(&arr4[j],&arr4[j+1]);
+    for(i=0; i<n; i++)
+        for(j=0; j<n; j++)
+        {
+            if(p[i]<p[j])
+            {
+                temp=p[i];
+                p[i]=p[j];
+                p[j]=temp;
+                temp=at[i];
+                at[i]=at[j];
+                at[j]=temp;
+                temp=et[i];
+                et[i]=et[j];
+                et[j]=temp;
+                strcpy(t,pn[i]);
+                strcpy(pn[i],pn[j]);
+                strcpy(pn[j],t);
             }
         }
+    for(i=0; i<n; i++)
+ 
+    {
+ 
+        if(i==0)
+        {
+            st[i]=at[i];
+            wt[i]=st[i]-at[i];
+            ft[i]=st[i]+et[i];
+            ta[i]=ft[i]-at[i];
+        }
+        else
+        {
+            st[i]=ft[i-1];
+            wt[i]=st[i]-at[i];
+            ft[i]=st[i]+et[i];
+            ta[i]=ft[i]-at[i];
+        }
+        totwt+=wt[i];
+        totta+=ta[i];
     }
+    awt=(float)totwt/n;
+    ata=(float)totta/n;
+    printf("\nPname\tarrivaltime\texecutiontime\tpriority\twaitingtime\ttatime");
+    for(i=0; i<n; i++)
+        printf("\n%s\t%5d\t\t%5d\t\t%5d\t\t%5d\t\t%5d",pn[i],at[i],et[i],p[i],wt[i],ta[i]);
+    printf("\nAverage waiting time is:%f",awt);
+    printf("\nAverage turnaroundtime is:%f",ata);
+    getch();
 }
